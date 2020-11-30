65893c095dfb24168ace6e929ba1f45f72bd5f81
krashr-ds <kylierrn@gmail.com>
2020-11-29 19:22:06 -0500

    Homework 6 - Bug fixes

--- homework6.py
+++ homework6.py
@@ -49,9 +49,9 @@ df6 = df5.reset_index(drop=True)
 df7 = df4[(df4["LEAGUE"] == "AL") & (df4["2B"] > 30)]
 
 # 7. From the merged data in part 3, create a new data frame with players that have > 5 triples OR > 30 runs
-# How many meet criteria? All 47.
+# How many meet criteria? Fifteen.
 
-df8 = df4[(df4["3B"] > 5) | (df4["R"] > 30)]
+df8 = df4[(df4["3B"] > 5) | (df4["HR"] > 30)]
 
 # 8. From the merged data in part 3, create a new column called 'avg' that contains each player's average (h / ab)
 
@@ -60,6 +60,7 @@ df4["AVG"] = df4["H"] / df4["AB"]
 # 9. Sort the data frame from part 8 descending by 'avg'
 
 df9 = df4.sort_values(by="AVG", ascending=False)
+df9 = df9.iloc[0:10]
 
 # 10. From the data frame in part 9, create a new data frame that only contains: First Last R 3B and RBI
 
